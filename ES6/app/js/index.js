34aeb3a9ebe72bd76ebadc559d17221ccaaa88df
chenshisong <Jerry@capsheaf.com>
2017-10-16 15:45:55 +0800

    learn some es6 basic

--- ES6/app/js/index.js
+++ ES6/app/js/index.js
@@ -1,2 +1,10 @@
-// import 'babel-polyfill';
+// es7兼容包
+import 'babel-polyfill';
 // import Lottery from './lottery';
+// import './class/lesson1'
+// import './class/lesson2'
+// import './class/lesson3'
+import './class/lesson4'
+// import './class/lesson5'
+// import './class/lesson6'
+// import './class/lesson7'
