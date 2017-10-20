f3e311377b66a42292e6dd3e563e88d222f8e5a6
chenshisong <Jerry@capsheaf.com>
2017-10-20 09:14:37 +0800

    add set map ...

--- ES6/app/js/index.js
+++ ES6/app/js/index.js
@@ -1,5 +1,5 @@
 // es7兼容包
-import 'babel-polyfill';
+// import 'babel-polyfill';
 // import Lottery from './lottery';
 // import './class/lesson1'
 // import './class/lesson2'
@@ -7,4 +7,9 @@ import 'babel-polyfill';
 // import './class/lesson4'
 // import './class/lesson5'
 // import './class/lesson6'
-import './class/lesson7'
+// import './class/lesson7'
+// import './class/lesson8'
+// import './class/lesson9'
+// import './class/lesson10'
+import './class/lesson11'
+// import './class/lesson12'
