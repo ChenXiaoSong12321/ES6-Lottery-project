8db30bd2692c57dae69c523a7c5148c94b802c71
Jerry12138 <448216252@qq.com>
2017-10-24 22:35:18 +0800

    add pages

--- ES6/app/js/index.js
+++ ES6/app/js/index.js
@@ -1,6 +1,6 @@
 // es7兼容包
-// import 'babel-polyfill';
-// import Lottery from './lottery';
+import 'babel-polyfill';
+import Lottery from './lottery';
 // import './class/lesson1'
 // import './class/lesson2'
 // import './class/lesson3'
